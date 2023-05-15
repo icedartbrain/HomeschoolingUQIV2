1f2ee6b62291c42746a8be9594f3ef5b49ae41de
icedartbrain <wahyubudiprastowo@gmail.com>
2023-05-15 10:00:56 +0700

    Initial commit

---
+++ app/src/main/java/com/hsuqi/homeschoolinguqiv2/MainActivity.java
@@ -0,0 +1,57 @@
+package com.hsuqi.homeschoolinguqiv2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomWebViewClient client = new CustomWebViewClient(this);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(client);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://app.hsuqi.com");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(keyCode == KeyEvent.KEYCODE_BACK && this.webView.canGoBack()){
+            this.webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
+
+class CustomWebViewClient extends WebViewClient{
+    private Activity activity;
+    public CustomWebViewClient(Activity activity){
+        this.activity = activity;
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, String url){
+        return false;
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, WebResourceRequest request){
+        return false;
+    }
+}
